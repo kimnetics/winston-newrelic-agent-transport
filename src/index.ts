bdab744d1139954b8749733e1dadab336230ce43
Gregory Kim <gkim@kimnetics.com>
2023-12-20 15:31:12 -0700

    Add message reject option.

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,77 @@ import newrelic from 'newrelic'
 import { LEVEL, MESSAGE } from 'triple-beam'
 import Transport, { type TransportStreamOptions } from 'winston-transport'
 
+export interface NewrelicTransportOptions extends TransportStreamOptions {
+  rejectCriteria?: RejectCriteria
+}
+
+export type RejectCriteria = RejectCriteriaItem[]
+
+export interface RejectCriteriaItem {
+  property: string | null
+  regex: string
+}
+
 export default class NewrelicTransport extends Transport {
   public readonly name: string
+  private readonly rejectCriteria?: RejectCriteria
 
-  constructor (options: TransportStreamOptions = {}) {
+  constructor (options: NewrelicTransportOptions = {}) {
     super(options)
     this.name = 'winston-newrelic-agent-transport'
     this.level = options.level ?? 'info'
+    this.rejectCriteria = options.rejectCriteria
   }
 
-  log (info: any, callback: () => void): void {
+  log (info: Record<string | symbol, any>, callback: () => void): void {
     setImmediate(() => {
       this.emit('logged', info)
     })
 
     try {
-      newrelic.recordLogEvent({
-        message: info[MESSAGE],
-        level: info[LEVEL]
-      })
+      const message = info[MESSAGE] as string
+      if ((this.rejectCriteria === undefined) || (!this.reject(info, message, this.rejectCriteria))) {
+        newrelic.recordLogEvent({
+          message,
+          level: info[LEVEL]
+        })
+      }
     } catch (err) {
     } finally {
       callback()
     }
+  }
+
+  reject (info: Record<string, any>, message: string, rejectCriteria: RejectCriteria): boolean {
+    if (rejectCriteria.length === 0) {
+      return false
+    }
+
+    for (const item of rejectCriteria) {
+      let value
+      if (item.property === null) {
+        value = message
+      } else {
+        value = this.getValue(info, item.property)
+      }
+
+      if ((value === undefined) || (value === null) || (typeof value !== 'string')) {
+        return false
+      }
+
+      if (value.match(item.regex) === null) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  getValue (info: Record<string, any>, property: string): Record<string, any> | string | undefined {
+    return property
+      .replace(/\[([^\]]*)]/g, '.$1')
+      .split('.')
+      .filter(p => p !== '')
+      .reduce((accumulator: Record<string, any> | string | undefined, current: string) => { return (typeof accumulator === 'string' ? undefined : accumulator?.[current]) }, info)
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import sinon from 'sinon'
 import newrelic from 'newrelic'
 import { LEVEL, MESSAGE } from 'triple-beam'
 
-import NewrelicTransport from '../src/index'
+import NewrelicTransport, { type RejectCriteriaItem } from '../src/index'
 
 describe('NewrelicTransport', () => {
   describe('constructor', () => {
@@ -27,7 +27,8 @@ describe('NewrelicTransport', () => {
   })
 
   describe('log', () => {
-    it('should log message and level to New Relic', () => {
+    it('should log message and level to New Relic if rejectCriteria is undefined', () => {
+      const transport = new NewrelicTransport()
       const info = {
         [MESSAGE]: 'test message',
         [LEVEL]: 'debug'
@@ -35,7 +36,6 @@ describe('NewrelicTransport', () => {
       const callback = sinon.stub()
       const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
 
-      const transport = new NewrelicTransport()
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       transport.log(info, callback)
 
@@ -48,16 +48,19 @@ describe('NewrelicTransport', () => {
       sinon.restore()
     })
 
-    it('should handle errors', () => {
+    it('should log message and level to New Relic if rejectCriteria is defined but does not match message', () => {
+      const transport = new NewrelicTransport({
+        rejectCriteria: [
+          { property: null, regex: 'nomatch' }
+        ]
+      })
       const info = {
         [MESSAGE]: 'test message',
         [LEVEL]: 'debug'
       }
       const callback = sinon.stub()
       const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
-      recordLogEventStub.throws(new Error('Error recording log event'))
 
-      const transport = new NewrelicTransport()
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       transport.log(info, callback)
 
@@ -68,6 +71,216 @@ describe('NewrelicTransport', () => {
       })
       sinon.assert.calledOnce(callback)
       sinon.restore()
+    })
+
+    it('should log message and level to New Relic if rejectCriteria is defined but does not match info object', () => {
+      const transport = new NewrelicTransport({
+        rejectCriteria: [
+          { property: 'property', regex: 'nomatch' }
+        ]
+      })
+      const info = {
+        property: 'test property',
+        [MESSAGE]: 'test message',
+        [LEVEL]: 'debug'
+      }
+      const callback = sinon.stub()
+      const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      transport.log(info, callback)
+
+      sinon.assert.calledOnce(recordLogEventStub)
+      sinon.assert.calledWith(recordLogEventStub, {
+        message: info[MESSAGE],
+        level: info[LEVEL]
+      })
+      sinon.assert.calledOnce(callback)
+      sinon.restore()
+    })
+
+    it('should not log message and level to New Relic if rejectCriteria is defined and matches message', () => {
+      const transport = new NewrelicTransport({
+        rejectCriteria: [
+          { property: null, regex: 'test message' }
+        ]
+      })
+      const info = {
+        [MESSAGE]: 'test message',
+        [LEVEL]: 'debug'
+      }
+      const callback = sinon.stub()
+      const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      transport.log(info, callback)
+
+      sinon.assert.notCalled(recordLogEventStub)
+      sinon.assert.calledOnce(callback)
+      sinon.restore()
+    })
+
+    it('should not log message and level to New Relic if rejectCriteria is defined and matches info object', () => {
+      const transport = new NewrelicTransport({
+        rejectCriteria: [
+          { property: 'property', regex: 'test property' }
+        ]
+      })
+      const info = {
+        property: 'test property',
+        [MESSAGE]: 'test message',
+        [LEVEL]: 'debug'
+      }
+      const callback = sinon.stub()
+      const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      transport.log(info, callback)
+
+      sinon.assert.notCalled(recordLogEventStub)
+      sinon.assert.calledOnce(callback)
+      sinon.restore()
+    })
+
+    it('should handle errors', () => {
+      const transport = new NewrelicTransport()
+      const info = {
+        [MESSAGE]: 'test message',
+        [LEVEL]: 'debug'
+      }
+      const callback = sinon.stub()
+      const recordLogEventStub = sinon.stub(newrelic, 'recordLogEvent')
+      recordLogEventStub.throws(new Error('Error recording log event'))
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      transport.log(info, callback)
+
+      sinon.assert.calledOnce(recordLogEventStub)
+      sinon.assert.calledWith(recordLogEventStub, {
+        message: info[MESSAGE],
+        level: info[LEVEL]
+      })
+      sinon.assert.calledOnce(callback)
+      sinon.restore()
+    })
+  })
+
+  describe('reject', () => {
+    it('should return false when rejectCriteria is an empty array', () => {
+      const transport = new NewrelicTransport()
+      const info = {}
+      const message = 'test message'
+      const rejectCriteria: RejectCriteriaItem[] = []
+
+      const result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(false)
+    })
+
+    it('should return true when all rejectCriteria match the message string', () => {
+      const transport = new NewrelicTransport()
+      const info = {}
+      const message = 'test message'
+      const rejectCriteria = [
+        { property: null, regex: 'test' },
+        { property: null, regex: 'message' }
+      ]
+
+      const result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(true)
+    })
+
+    it('should return true when all rejectCriteria match the value of the specified property in the info object', () => {
+      const transport = new NewrelicTransport()
+      const info = { property: 'test property' }
+      const message = ''
+      const rejectCriteria = [
+        { property: 'property', regex: 'test' },
+        { property: 'property', regex: 'property' }
+      ]
+
+      const result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(true)
+    })
+
+    it('should return false if any rejectCriteria do not match the message string', () => {
+      const transport = new NewrelicTransport()
+      const info = {}
+      const message = 'test message'
+
+      let rejectCriteria = [
+        { property: null, regex: 'nomatch' },
+        { property: null, regex: 'message' }
+      ]
+      let result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(false)
+
+      rejectCriteria = [
+        { property: null, regex: 'test' },
+        { property: null, regex: 'nomatch' }
+      ]
+      result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(false)
+    })
+
+    it('should return false if any rejectCriteria do not match the value of the specified property in the info object', () => {
+      const transport = new NewrelicTransport()
+      const info = { property: 'test property' }
+      const message = ''
+
+      let rejectCriteria = [
+        { property: 'property', regex: 'nomatch' },
+        { property: 'property', regex: 'property' }
+      ]
+      let result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(false)
+
+      rejectCriteria = [
+        { property: 'property', regex: 'test' },
+        { property: 'property', regex: 'nomatch' }
+      ]
+      result = transport.reject(info, message, rejectCriteria)
+      expect(result).to.equal(false)
+    })
+  })
+
+  describe('getValue', () => {
+    it('should return the value of a property in an object when given a valid property string', () => {
+      const transport = new NewrelicTransport()
+      const object = { foo: { bar: 'baz' } }
+
+      let property = 'foo.bar'
+      let result = transport.getValue(object, property)
+      expect(result).to.equal('baz')
+
+      property = '[foo][bar]'
+      result = transport.getValue(object, property)
+      expect(result).to.equal('baz')
+
+      property = 'foo[bar]'
+      result = transport.getValue(object, property)
+      expect(result).to.equal('baz')
+
+      property = 'foo.[bar]'
+      result = transport.getValue(object, property)
+      expect(result).to.equal('baz')
+    })
+
+    it('should return undefined when given an invalid property string', () => {
+      const transport = new NewrelicTransport()
+      const object = { foo: { bar: 'baz' } }
+      const property = 'foo.baz'
+
+      const result = transport.getValue(object, property)
+      expect(result).to.equal(undefined)
+    })
+
+    it('should handle property strings with non-existent nested properties correctly', () => {
+      const transport = new NewrelicTransport()
+      const object = { foo: { bar: 'baz' } }
+      const property = 'foo.baz.qux'
+
+      const result = transport.getValue(object, property)
+      expect(result).to.equal(undefined)
     })
   })
 })
