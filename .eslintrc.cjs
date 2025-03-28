f1503826af0c9b3d7a8b5c12e2cb1085bd295c13
Gregory Kim <gkim@kimnetics.com>
2025-03-27 22:20:52 -0600

    Update packages.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,6 @@ module.exports = {
     node: true
   },
   extends: 'standard-with-typescript',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
